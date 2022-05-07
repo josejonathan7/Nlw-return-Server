f9ecfc90c54c15f3fe83ed78cc10d9500f2018e3
jose jonathan <josejonathan689@gmail.com>
2022-05-07 14:47:09 -0300

    configurando alguns detalhes

--- src/use-cases/submitFeedbackUseCases.ts
+++ src/use-cases/submitFeedbackUseCases.ts
@@ -44,6 +44,7 @@ export class SubmitFeedbackUseCases {
 				'<div style="font-family: sans-serif; font-size: 16px; color: #111;">',
 				`<p>Tipo do feedback ${type}</p>`,
 				`<p>Comentário: ${comment}</p>`,
+				screenshot ? `<img source="${screenshot}" />` : '',
 				'</div>'
 			].join('\n')
 		});
